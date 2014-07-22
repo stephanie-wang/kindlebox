f8eb1a6b2c3851bf6934e731a86f8843d521dd57
stephanie <swang93@mit.edu>
2014-07-22 01:03:49 -0700

    move to postgres

--- kindlebox/database.py
+++ kindlebox/database.py
@@ -2,8 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+import constants
+
 # TODO(sxwang): switch to postgres :(
-engine = create_engine('sqlite:////tmp/kindlebox.db', convert_unicode=True)
+engine = create_engine('postgresql://%s:%s@localhost/kindlebox' %
+        (constants.POSTGRES_USER, constants.POSTGRES_PASSWORD),
+        convert_unicode=True)
 db = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
