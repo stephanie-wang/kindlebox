1da165d97dfa7a7ef0b1ab1dc9bd021f10abd5cf
stephanie <swang93@mit.edu>
2014-07-22 01:21:55 -0700

    settings not constants

--- kindlebox/database.py
+++ kindlebox/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-import constants
+import settings
 
 # TODO(sxwang): switch to postgres :(
 engine = create_engine('postgresql://%s:%s@localhost/kindlebox' %
-        (constants.POSTGRES_USER, constants.POSTGRES_PASSWORD),
+        (settings.POSTGRES_USER, settings.POSTGRES_PASSWORD),
         convert_unicode=True)
 db = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
