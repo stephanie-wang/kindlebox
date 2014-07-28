61903379ebff2c05cf45e8d64c2eb0b16f5d323f
stephanie <swang93@mit.edu>
2014-07-28 00:52:16 -0700

    some styles and amazon instructions

--- static/js/kindlebox.js
+++ static/js/kindlebox.js
@@ -5,9 +5,11 @@ function showEmailer(emailer) {
         <div class="instruction-num"> \
           3. \
         </div> \
-        <div class="instruction"> \
-          Your Kindlebox emailer is:  \
+        <div id="emailer-text" class="instruction"> \
+          Kindlebox works by emailing the books in your Dropbox folder to your Kindle. Here\'s your Kindlebox emailer: \
           <pre class="instruction-action">' + emailer + '</pre> \
+          <p>To start receiving books through Kindlebox, visit <a href="https://www.amazon.com/manageyourkindle">Manage Your Content and Devices</a> at amazon.com. Go to the <b>Settings</b> tab, scroll down to <b>Personal Document Settings</b>, and add the above email address to your <b>Approved Personal Document E-mail List</b>.</p> \
+          <p>Finally, <a href="/activate" method="post">activate Kindlebox</a>.</p> \
         </div> \
       </div> \
       ';
@@ -17,18 +19,23 @@ function showEmailer(emailer) {
 }
 
 $(function() {
+  var kindleComWidth = $('#kindle-com').outerWidth();
+  var formWidth = $('#user-info-form').innerWidth();
+  $('#kindle-name').width(formWidth - kindleComWidth - 40);
   // Show the emailer instruction after successfully submitting a Kindle
   // username.
   $('#user-info-form').submit(function(evt) {
     evt.preventDefault();
     var kindleName = $('#kindle-name').val();
+    if (kindleName.length == 0) {
+      return;
+    }
     $.post('/set-user-info', {
       'kindle_name': kindleName
     }, function(data) {
       if (data.success) {
         showEmailer(data.emailer);
         $(':focus').blur();
-        $('#kindle-name-instruction').addClass('instruction-completed');
       }
     });
   });
@@ -42,5 +49,7 @@ $(function() {
     }
   }).focus(function() {
     $('#kindle-name-instruction').removeClass('instruction-completed');
+  }).blur(function() {
+    $('#kindle-name-instruction').addClass('instruction-completed');
   });
 });
