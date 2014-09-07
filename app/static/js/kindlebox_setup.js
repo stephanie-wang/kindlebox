5445949b43be7f7c3305e1f35539e3ded740151d
stephanie <swang93@mit.edu>
2014-09-06 21:20:51 -0400

    and setup csrf

---
+++ app/static/js/kindlebox_setup.js
@@ -0,0 +1,7 @@
+$.ajaxSetup({
+  beforeSend: function(xhr, settings) {
+    if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+      xhr.setRequestHeader("X-CSRFToken", kindleboxCsrfToken)
+    }
+  }
+});
